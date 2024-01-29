f6907c4bf8761b991f96152d9f0cad72e2796a84
Franco Traversaro <franco.traversaro@nan.srl>
2024-01-29 18:08:23 +0100

    Bug example

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Audio } from "expo-av";
+import { useCallback } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const playSound = useCallback(async () => {
+    const sound = await Audio.Sound.createAsync(require("./assets/beep.mp3"));
+    await sound.sound.playAsync();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>
+        A couple of warnings get logged as soon this View is rendered.
+      </Text>
+      <Text style={styles.text}>
+        Another couple appears as long the playSound() callback is executed.
+      </Text>
+      <Button title="Play Sound" onPress={playSound} />
     </View>
   );
 }
@@ -13,8 +24,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    rowGap: 10,
+    margin: 50,
+    textAlign: "center",
+  },
+  text: {
+    textAlign: "center",
   },
 });
